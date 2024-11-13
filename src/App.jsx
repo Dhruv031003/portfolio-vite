3b1f5ef8e3fe115a578ae303837e7e9cb443b841
dhruvdorbi <dhruvdorbi03@gmail.com>
2024-11-13 20:42:35 +0530

    added contact section and changed title

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ function App() {
     <>
       <div className='h-screen w-full flex flex-col justify-center items-between'>
         <div className="mx-auto flex flex-col justify-center items-center">
-          <h1 className="w-fit londrinaSketch text-9xl grad-bg">COMING SOON!!!</h1>
+          <h1 className="mx-auto text-center w-fit londrinaSketch text-9xl grad-bg">COMING SOON!!!</h1>
+        </div>
+        <div className="flex justify-around mt-4 w-full md:w-1/2 mx-auto">
+          {/* Linkedin */}
+          <a className="mx-1.5 transition-colors duration-300 transform hover:scale-110" href="https://www.linkedin.com/in/dhruv-dorbi/" target='blank_'>
+            <svg className="w-8 h-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.2 8.80005C16.4731 8.80005 17.694 9.30576 18.5941 10.2059C19.4943 11.1061 20 12.327 20 13.6V19.2H16.8V13.6C16.8 13.1757 16.6315 12.7687 16.3314 12.4687C16.0313 12.1686 15.6244 12 15.2 12C14.7757 12 14.3687 12.1686 14.0687 12.4687C13.7686 12.7687 13.6 13.1757 13.6 13.6V19.2H10.4V13.6C10.4 12.327 10.9057 11.1061 11.8059 10.2059C12.7061 9.30576 13.927 8.80005 15.2 8.80005Z" fill="currentColor" />
+              <path d="M7.2 9.6001H4V19.2001H7.2V9.6001Z" fill="currentColor" />
+              <path d="M5.6 7.2C6.48366 7.2 7.2 6.48366 7.2 5.6C7.2 4.71634 6.48366 4 5.6 4C4.71634 4 4 4.71634 4 5.6C4 6.48366 4.71634 7.2 5.6 7.2Z" fill="currentColor" />
+            </svg>
+          </a>
+          <a className="mx-1.5 transition-colors duration-300 transform hover:scale-110" href="https://github.com/Dhruv031003" target='blank_'>
+            <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+              <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+            </svg>
+          </a>
         </div>
       </div>
     </>
