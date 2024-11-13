6e5da8e7e4ae2da4ea005d079a4d061302ecd74f
dhruvdorbi <dhruvdorbi03@gmail.com>
2024-11-13 17:44:46 +0530

    coming soon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 
 function App() {
-
   return (
     <>
-      <h1 className="text-center bg-slate-400">hello there</h1>
+      <div className='h-screen w-full flex flex-col justify-center items-between'>
+        <div className="mx-auto flex flex-col justify-center items-center">
+          <h1 className="w-fit londrinaSketch text-9xl grad-bg">COMING SOON!!!</h1>
+        </div>
+      </div>
     </>
   )
 }
